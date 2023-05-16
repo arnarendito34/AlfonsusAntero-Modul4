b958bc7da53775c687d2d85c02f3c7b3cebfe302
arnarendito34 <renditobloggerspot@gmail.com>
2023-05-16 23:35:17 +0700

    AlfonsusAntero-Modul4

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = 'Employee List';
+
+        return view('employee.index', ['pageTitle' => $pageTitle]);
+    }
+
+    public function create()
+    {
+        $pageTitle = 'Create Employee';
+
+        return view('employee.create', compact('pageTitle'));
+    }
+
+    public function store(Request $request)
+{
+    $messages = [
+        'required' => ':Attribute harus diisi.',
+        'email' => 'Isi :attribute dengan format yang benar',
+        'numeric' => 'Isi :attribute dengan angka'
+    ];
+
+    $validator = Validator::make($request->all(), [
+        'firstname' => 'required',
+        'lastname' => 'required',
+        'email' => 'required|email',
+        'age' => 'required|numeric',
+    ], $messages);
+
+    if ($validator->fails()) {
+        return redirect()->back()->withErrors($validator)->withInput();
+    }
+
+    return $request->all();
+}
+}
